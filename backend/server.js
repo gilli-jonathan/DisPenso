5d7a0622cb64061745331e88e583a85f21db2c18
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-21 12:45:36 +0200

    sperem pt.3

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('ciao');
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Server Attivo!!!")
+})
+
+app.listen(3000, () => {
+    console.log("Server attivo su porta: 3000");
+})
