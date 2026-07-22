26a40ef530cda0a89b39179c04b439d17c1d440c
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-07-22 11:46:50 +0200

    add supabase

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+const db = require('../config/db');
+
+
+const index = async (req, res) => {
+    try {
+
+        const { data, error } = await db.from('product').select('*');
+
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+
+        res.json(data);
+
+    } catch (err) {
+        res.status(500).json({ error: "Errore imprevisto del server" });
+    }
+};
+
+module.exports = {
+    index
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
-const express = require("express")
+// 1. Carichiamo le variabili d'ambiente il prima possibile
+require('dotenv').config();
 
-const app = express()
+// 2. Importiamo express dal pacchetto installato tramite NPM
+const express = require('express');
 
-app.use(express.json())
+// 3. Inizializziamo l'oggetto app
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server Attivo!!!")
-})
+// 4. Definiamo la porta su cui il server rimarrà in ascolto
+const port = 3000;
 
-app.listen(3000, () => {
-    console.log("Server attivo su porta: 3000");
-})
+// 5. Definiamo la nostra prima rotta di test
+app.get('/', (req, res) => {
+    res.send('Il server Express è online e pronto!');
+});
+
+// 6. Avviamo il server
+app.listen(port, () => {
+    console.log(`App in ascolto sulla porta ${port}`);
+});
+
+// ... (codice precedente: require dotenv, express, const app = express())
+
+// 1. Importiamo il router appena creato
+const productRouter = require('./routers/productRouter');
+
+// 2. Diciamo ad Express: "Tutte le richieste che iniziano con /alimenti, passale ad alimentiRouter"[cite: 6]
+app.use('/product', productRouter);
+
+// ... (codice successivo: app.get di test e app.listen)
